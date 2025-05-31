5ddfb1aedc07dffb95286de32192ae0c9ca980de
Redian Fikri <redianmf@gmail.com>
2025-05-31 13:18:47 +0700

    feat: implement uuid

--- src/functions/InsertTask.ts
+++ src/functions/InsertTask.ts
@@ -4,6 +4,7 @@ import {
   HttpResponseInit,
   InvocationContext,
 } from "@azure/functions";
+import { v4 as UUIDv4 } from "uuid";
 import { dbClient } from "../database";
 
 export async function InsertTask(
@@ -12,10 +13,15 @@ export async function InsertTask(
 ): Promise<HttpResponseInit> {
   const body = await request.json();
 
+  if (!body) return { status: 400 };
+  const payload = { ...(body as object), id: UUIDv4() };
+
+  console.log(payload);
+
   const createdTask = await dbClient
     .database("TaskApp")
     .container("Tasks")
-    .items.create(body);
+    .items.create(payload);
 
   return { jsonBody: createdTask.resource, status: 200 };
 }
